da7d3c09f6af29fff3dfbac87026630cf723ff61
kaneko <kaneko@e2info.com>
2020-03-22 20:09:23 +0900

    サンプルコード追加

---
+++ src/app/Http/Controllers/SampleController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SampleController extends Controller
+{
+    public function sample()
+    {
+        print 1;
+            print 2;
+                print 3;
+                    print 4;
+    }
+
+    public function sample2(){
+        print 1;
+        print 2;
+    }
+
+    public function sample3(){
+        print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;print 1;
+    }
+}
