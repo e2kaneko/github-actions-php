96f09485146d72a83723b71a4690aaceeccb1841
kaneko <kaneko@e2info.com>
2020-03-23 16:56:05 +0900

    サンプル用のテストケース追加

---
+++ src/tests/Feature/SampleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class SampleTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertEquals("2019", "2019");
+        $this->assertEquals("2020", "2020");
+        $this->assertEquals("2021", "2021");
+    }
+}
